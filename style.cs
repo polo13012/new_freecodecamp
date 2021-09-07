c9a993fd72f3ef2ef0e0fc53cb4bfe2b9b86635b
chalvet paul <90175708+polo13012@users.noreply.github.com>
2021-09-07 11:49:02 +0200

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,82 @@
+*{top:0;}
+h1{text-align:center;
+  padding-top:60px;
+}
+#form{text-align:center;
+}
+#email{margin-bottom:10px
+}
+#nav-bar{top:0px;;margin:0px 00px 0px -400px;
+  display:flex;
+  
+  justify-content: space-evenly;
+}
+#header{width:100%;
+  position:fixed;
+}
+
+
+
+
+
+
+.nav-link{
+  text-decoration:none;
+  font-size:19px;
+  color:black;
+  padding-top:15px;
+ 
+}
+#header-img{
+  width:300px;
+padding-top:1px;
+  opacity:0.6;
+}
+h2{
+  margin-top:10px;
+  opacity:0.7;
+  display:flex;
+  justify-content:flex-start;
+  
+  
+  }
+#main{text-align:center;}
+p{
+  margin-left:390px;
+  
+  
+}
+
+  
+h4{margin-left:-340px;
+}
+span{font-size:20px;
+}
+.title{background-color:grey;
+}
+#acards{
+border-style: solid;
+  width:188px;
+  height:250px;
+  margin-top:10px;
+  
+}
+#bcards{border-style: solid;
+  width:188px;
+  height:250px;
+  margin:10px;
+  
+}
+#ccards{
+  border-style: solid;
+  width:188px;
+  height:250px;
+  margin-top:10px;
+}
+.cards{display:flex;
+  justify-content:center;
+}
+.button{background-color:yellow;
+}
+.button:hover {background-color:rgb(20,255,20);}
+@media screen and(min-width:200px){}
